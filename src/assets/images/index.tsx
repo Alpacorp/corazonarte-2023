3052a7936f028732e1acf3f8087560f8d55ce437
Alpacorp <alejandro.palacios88@gmail.com>
2023-04-23 19:32:09 -0500

    test images fix source

--- src/assets/images/index.tsx
+++ src/assets/images/index.tsx
@@ -1,3 +1,27 @@
 import portfolioLogo from "./portfolio-logo.svg";
+import artesanias from "./artesanias.svg";
+import background from "./background.svg";
+import cine from "./cine.svg";
+import cundinamarcaRegion from "./cundinamarca-region-logo.svg";
+import gobernacionCun from "./gobernacion-cundinamarca-logo.svg";
+import idecut from "./idecut-logo.svg";
+import libro from "./libro.svg";
+import musica from "./musica.svg";
+import pintura from "./pintura.svg";
+import teatro from "./teatro.svg";
+import tejidos from "./tejidos.svg";
 
-export { portfolioLogo };
+export {
+  portfolioLogo,
+  artesanias,
+  background,
+  cine,
+  cundinamarcaRegion,
+  gobernacionCun,
+  idecut,
+  libro,
+  musica,
+  pintura,
+  teatro,
+  tejidos,
+};
